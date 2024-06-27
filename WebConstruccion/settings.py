f18b6ef2bec00777fe298a513c9b3f72b64754ed
JerickTwO <jerickjjtm774@gmail.com>
2024-06-27 16:51:14 -0500

    postgressql

--- WebConstruccion/settings.py
+++ WebConstruccion/settings.py
@@ -1,6 +1,8 @@
 import os
-
+import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,12 +67,9 @@ WSGI_APPLICATION = 'WebConstruccion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+print(os.getenv('DATABASE_URL'))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default="os.getenv('DATABASE_URL')")
 }
 
 
