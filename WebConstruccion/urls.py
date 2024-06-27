d14e92d3ef1fbe16395e1e29b43fd64e54ac505c
JerickTwO <jerickjjtm774@gmail.com>
2024-06-27 17:03:35 -0500

    name.secure

--- WebConstruccion/urls.py
+++ WebConstruccion/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('panel-admin/', admin.site.urls),
     path('products/', include('products.urls')),
 ]
 
