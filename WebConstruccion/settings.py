f15f90595010cd0919216d54455a5da7c7b86e43
JerickTwO <jerickjjtm774@gmail.com>
2024-06-27 17:13:48 -0500

    link and repo

--- WebConstruccion/settings.py
+++ WebConstruccion/settings.py
@@ -121,5 +121,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','web-production-1826d.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://web-production-1826d.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','https://jhg-builder.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://https://jhg-builder.up.railway.app']
